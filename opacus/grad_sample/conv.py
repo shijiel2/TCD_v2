c0a1c4bc23f115b76d7f23abb99e9bddaae9c13c
Alexandre Sablayrolles <asablayrolles@fb.com>
2021-11-05 13:32:54 +0100

    Implementation of conv2d that works

--- opacus/grad_sample/conv.py
+++ opacus/grad_sample/conv.py
@@ -27,7 +27,7 @@ def compute_conv_grad_sample(
         backprops: Backpropagations
     """
     # TODO: No clue what is up when stride is not (1, 1)
-    # if layer.stride == (1, 1):
+
     # Things are hardcoded for conv2d at the moment
     assert len(layer.stride) == 2
     assert len(layer.padding) == 2
@@ -35,38 +35,21 @@ def compute_conv_grad_sample(
 
     batch_size = activations.shape[0]
     I = activations.shape[1]
-    O = backprops.shape[1]
 
-    
-    print("ACTIVATIONS", activations.shape)
-    print("BACKPROPS", backprops.shape)
-    print("WEIGHT", layer.weight.shape)
-
-    print("stride", layer.stride)
-    print("dilation", layer.dilation)
-    print("padding", layer.padding)
     # activations has shape (B, I, H, W)
     # backprops has shape (B, O, H, W)
     activations_ = activations.view(batch_size, layer.groups, I // layer.groups, activations.shape[2], activations.shape[3]) # (B, G, I/G, H, W)
 
-    # import ipdb;ipdb.set_trace()
-
-    # activations_ = activations.transpose(0, 1) # (I, B, H, W)
     activations_ = activations_.view(activations_.shape[0] * activations_.shape[1], activations_.shape[2], activations_.shape[3], activations_.shape[4]) # (B*G, I / G, H, W)
     activations_ = activations_.transpose(0, 1) # (I / G, B * G, H, W)
     backprops_ = backprops.view(backprops.shape[0] * backprops.shape[1], 1, backprops.shape[2], backprops.shape[3]) # (B*O, 1, H, W)
 
-    print("ACTIVATIONS_", activations_.shape)
-    print("BACKPROPS_", backprops_.shape)
-    # (I, B, H, W) X (B*O, 1, H, W) -> (I, B*O, H, W)
-
-    # (I / G, B*G, H, W) X (B*O, 1, H, W) -> (I / G, B * O, H, W)
-
+    # Without groups (I, B, H, W) X (B*O, 1, H, W) -> (I, B*O, H, W)
+    # With groups (I / G, B*G, H, W) X (B*O, 1, H, W) -> (I / G, B * O, H, W)
     weight_grad_sample = F.conv2d(activations_, backprops_, bias=None, dilation=layer.stride, padding=layer.padding, stride=layer.dilation, groups=batch_size * layer.groups)
-    weight_grad_sample = weight_grad_sample.view(I // layer.groups, batch_size, O, *weight_grad_sample.shape[-2:])
-    weight_grad_sample = weight_grad_sample.movedim(0, 2)
-
-    print("WEIGHT GRAD SAMPLE", weight_grad_sample.shape)
+    weight_grad_sample = weight_grad_sample.view(I // layer.groups, batch_size, O, *weight_grad_sample.shape[-2:]) # (I / G, B, O, H, W)
+    weight_grad_sample = weight_grad_sample.movedim(0, 2) # (B, O, I/G, H, W)
+    weight_grad_sample = weight_grad_sample[:, :, :, :layer.weight.shape[2], :layer.weight.shape[3]]
 
     ret = {
         layer.weight: weight_grad_sample
--- opacus/tests/grad_samples/conv2d_test.py
+++ opacus/tests/grad_samples/conv2d_test.py
@@ -8,6 +8,8 @@ import torch
 import torch.nn as nn
 from hypothesis import given, settings
 
+from opacus.grad_sample.grad_sample_module import GradSampleModule
+
 from .common import GradSampleHooks_test, expander, shrinker
 
 
@@ -15,11 +17,11 @@ class Conv2d_test(GradSampleHooks_test):
     @given(
         N=st.integers(1, 4),
         C=st.sampled_from([1, 3, 32]),
-        H=st.integers(6, 10),
-        W=st.integers(6, 10),
+        H=st.integers(8, 14),
+        W=st.integers(8, 14),
         out_channels_mapper=st.sampled_from([expander, shrinker]),
         kernel_size=st.integers(2, 4),
-        stride=st.sampled_from([1]),#st.integers(1, 2),
+        stride=st.integers(1, 2),
         padding=st.sampled_from([0, 1, 2, 3, 4]),
         dilation=st.sampled_from([1, 2, 3]),#st.integers(1, 2),
         groups=st.integers(1, 16),
@@ -56,3 +58,20 @@ class Conv2d_test(GradSampleHooks_test):
             groups=groups,
         )
         self.run_test(x, conv, batch_first=True, atol=10e-5, rtol=10e-4)
+
+
+
+    def test_conv2d_particular(self):
+        """
+        This test is mainly for particular use cases and can be useful for future debugging
+        """
+        x = torch.randn(1, 1, 8, 1)
+        layer = nn.Conv2d(1, 1, kernel_size=(3, 1), groups=1, dilation=(3, 1), bias=None)
+
+        m = GradSampleModule(layer)
+
+        y = m(x)
+        backprops = torch.randn(*y.shape)
+        y.backward(backprops)
+
+        self.assertLess(torch.norm(m._module.weight.grad_sample[0] - m._module.weight.grad), 1e-7)
